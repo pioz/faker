1a6f3e42c4263846d3716a5ea8478f7c80078deb
Enrico <epilotto@gmx.com>
2020-08-21 14:14:46 +0200

    Improve coverage

--- utils_test.go
+++ utils_test.go
@@ -2,6 +2,7 @@ package faker_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/pioz/faker"
 	"github.com/stretchr/testify/assert"
@@ -45,4 +46,76 @@ func TestParamsToMinMaxIntInvaliParams4(t *testing.T) {
 	err := faker.Build(&s)
 	assert.NotNil(t, err)
 	assert.Equal(t, "invalid parameters: strconv.Atoi: parsing \"b\": invalid syntax", err.Error())
+}
+
+func TestParamsToMinMaxFloat64InvaliParams1(t *testing.T) {
+	s := &struct {
+		Field float64 `faker:"Float64InRange"`
+	}{}
+	err := faker.Build(&s)
+	assert.NotNil(t, err)
+	assert.Equal(t, "invalid parameters", err.Error())
+}
+
+func TestParamsToMinMaxFloat64InvaliParams2(t *testing.T) {
+	s := &struct {
+		Field float64 `faker:"Float64InRange(1.0,b)"`
+	}{}
+	err := faker.Build(&s)
+	assert.NotNil(t, err)
+	assert.Equal(t, "invalid parameters: strconv.ParseFloat: parsing \"b\": invalid syntax", err.Error())
+}
+
+func TestParamsToMinMaxFloat64InvaliParams3(t *testing.T) {
+	s := &struct {
+		Field float64 `faker:"Float64InRange(a,2)"`
+	}{}
+	err := faker.Build(&s)
+	assert.NotNil(t, err)
+	assert.Equal(t, "invalid parameters: strconv.ParseFloat: parsing \"a\": invalid syntax", err.Error())
+}
+
+func TestParamsToMinMaxDurationInvalidParams1(t *testing.T) {
+	s := &struct {
+		Field time.Duration `faker:"DurationInRange"`
+	}{}
+	err := faker.Build(&s)
+	assert.NotNil(t, err)
+	assert.Equal(t, "invalid parameters", err.Error())
+}
+
+func TestParamsToMinMaxDurationInvalidParams2(t *testing.T) {
+	s := &struct {
+		Field time.Duration `faker:"DurationInRange(2s,b)"`
+	}{}
+	err := faker.Build(&s)
+	assert.NotNil(t, err)
+	assert.Equal(t, "invalid parameters: time: invalid duration b", err.Error())
+}
+
+func TestParamsToMinMaxDurationInvalidParams3(t *testing.T) {
+	s := &struct {
+		Field time.Duration `faker:"DurationInRange(a,2ms)"`
+	}{}
+	err := faker.Build(&s)
+	assert.NotNil(t, err)
+	assert.Equal(t, "invalid parameters: time: invalid duration a", err.Error())
+}
+
+func TestParamsToIntInvalidParams1(t *testing.T) {
+	s := &struct {
+		Field string `faker:"StringWithSize"`
+	}{}
+	err := faker.Build(&s)
+	assert.NotNil(t, err)
+	assert.Equal(t, "invalid parameters", err.Error())
+}
+
+func TestParamsToIntInvalidParams2(t *testing.T) {
+	s := &struct {
+		Field string `faker:"StringWithSize(a)"`
+	}{}
+	err := faker.Build(&s)
+	assert.NotNil(t, err)
+	assert.Equal(t, "invalid parameters: strconv.Atoi: parsing \"a\": invalid syntax", err.Error())
 }
