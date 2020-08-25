6bbdea202da4fdd2127dd1bb46bd5bec8b519696
Enrico <epilotto@gmx.com>
2020-08-25 23:00:24 +0200

    Add faker.Sample method

--- slice.go
+++ slice.go
@@ -1,5 +1,9 @@
 package faker
 
+import (
+	"reflect"
+)
+
 // Slice will build a random slice of interface{} of length n. The elements of
 // the slice are genereted by the function fn.
 func Slice(size int, fn func() interface{}) []interface{} {
@@ -8,4 +12,21 @@ func Slice(size int, fn func() interface{}) []interface{} {
 		slice = append(slice, fn())
 	}
 	return slice
+}
+
+// Sample return a random element of slice. Sample panics if slice is not a
+// slice, is nil or is empty.
+func Sample(slice interface{}) interface{} {
+	if reflect.TypeOf(slice).Kind() != reflect.Slice {
+		panic("faker.Sample param must be a slice")
+	}
+	sliceReflectValue := reflect.ValueOf(slice)
+	if sliceReflectValue.IsNil() {
+		panic("faker.Sample param is nil")
+	}
+	if sliceReflectValue.Len() == 0 {
+		panic("faker.Sample param is empty")
+	}
+	i := IntInRange(0, sliceReflectValue.Len()-1)
+	return sliceReflectValue.Index(i).Interface()
 }
--- slice_test.go
+++ slice_test.go
@@ -2,12 +2,41 @@ package faker_test
 
 import (
 	"fmt"
+	"testing"
 
 	"github.com/pioz/faker"
+	"github.com/stretchr/testify/assert"
 )
 
 func ExampleSlice() {
 	faker.SetSeed(200)
 	fmt.Println(faker.Slice(3, func() interface{} { return faker.IntInRange(0, 10) }))
 	// Output: [6 9 9]
+}
+
+func ExampleSample() {
+	faker.SetSeed(201)
+	slice := []int{1, 2, 3, 4, 5}
+	fmt.Println(faker.Sample(slice))
+	// Output: 5
+}
+
+func TestSampleWithNoSliceArg(t *testing.T) {
+	assert.PanicsWithValue(t, "faker.Sample param must be a slice", func() {
+		faker.Sample(2)
+	})
+}
+
+func TestSampleWithNilSlice(t *testing.T) {
+	var slice []int
+	assert.PanicsWithValue(t, "faker.Sample param is nil", func() {
+		faker.Sample(slice)
+	})
+}
+
+func TestSampleWithEmptySlice(t *testing.T) {
+	slice := make([]int, 0)
+	assert.PanicsWithValue(t, "faker.Sample param is empty", func() {
+		faker.Sample(slice)
+	})
 }
